1e13c3ed0edfdfa94f31566219bb76c8189b5e27
MichaelRol <michael.rollins@hotmail.co.uk>
2019-03-10 00:30:39 +0100

    It currently works probably won't last

--- app/src/main/java/com/example/sbbreusablecupchallenge/MainActivity.java
+++ app/src/main/java/com/example/sbbreusablecupchallenge/MainActivity.java
@@ -11,6 +11,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.util.Log;
+
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.json.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,9 +61,53 @@ public class MainActivity extends AppCompatActivity {
 
             if (resultCode == RESULT_OK) {
                 String contents = data.getStringExtra("SCAN_RESULT");
+                
                 startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(contents)));
             }
 
         }
     }
+
+    public void request() {
+        Log.d("HELLO", "request() called");
+        // Create okhttp3 request builder.
+        Request.Builder builder = new Request.Builder();
+        // Set url.
+        builder = builder.url("https://bxmbrpyq1h.execute-api.eu-west-2.amazonaws.com/Dev/dbupdate");
+        // Create request object.
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        char q = '"';
+        String jsonBody = "{"+q+"user_id"+q+":"+q+"USER_123456"+q+"}";
+        Log.d("HELLO",jsonBody);
+        String header = "hello world";
+
+        RequestBody body = RequestBody.create(JSON, jsonBody);
+
+        Request request = new Request.Builder()
+                .url("https://bxmbrpyq1h.execute-api.eu-west-2.amazonaws.com/Dev/dbupdate")
+                .put(body) //PUT
+                .addHeader("Authorization", header)
+                .build();
+
+        // Create a new Call object.
+        OkHttpClient client = new OkHttpClient();
+        Call call = client.newCall(request);
+
+        Log.d("HELLO", "about to enter the try");
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d("HELLO", "callback failure");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("HELLO", "callback response");
+                Log.d("HELLO", "reponse body is: " + response.body().string());
+            }
+        });
+    }
+
 }
+
